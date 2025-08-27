3fbbe98f2578968c48c7ecc97ea96abf6c72c3cc
Jeffrey Dao <jeffreydao7@gmail.com>
2025-08-26 19:04:55 -0500

    Add reset once 5 points is reached

--- script.js
+++ script.js
@@ -10,25 +10,51 @@ function getComputerChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+let isComplete = false;
 
 function playRound(humanChoice) {
+  if (isComplete) {
+    return "Game over! Please reset to play again.";
+  }
+
   console.log(`You chose: ${humanChoice}`);
   let computerChoice = getComputerChoice();
   console.log(`Computer chose: ${computerChoice}`);
 
+  let result = "";
   if (humanChoice === computerChoice) {
-    return `It's a tie! Current score: ${humanScore} to ${computerScore}`;
+    result = `It's a tie! Current score: ${humanScore} to ${computerScore}`;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     humanScore++;
-    return `You win! ${humanChoice} beats ${computerChoice}. Current score: ${humanScore} to ${computerScore}`;
+    if (humanScore === 5) {
+      isComplete = true;
+      result = `You won the match! Final score: ${humanScore} to ${computerScore}`;
+    } else {
+      result = `You win! ${humanChoice} beats ${computerChoice}. Current score: ${humanScore} to ${computerScore}`;
+    }
   } else {
     computerScore++;
-    return `You lose! ${computerChoice} beats ${humanChoice}. Current score: ${humanScore} to ${computerScore}`;
+    if (computerScore === 5) {
+      isComplete = true;
+      result = `You lost the match. Final score: ${humanScore} to ${computerScore}`;
+    } else {
+      result = `You lose! ${computerChoice} beats ${humanChoice}. Current score: ${humanScore} to ${computerScore}`;
+    }
   }
+  return result;
+}
+
+function resetGame() {
+    isComplete = false;
+    computerScore = 0;
+    humanScore = 0;
+    const result = "Resetting the game, select any button to play agian.";
+
+    return result
 }
 
 const resultDiv = document.getElementById("result");
@@ -50,3 +76,10 @@ scissors.addEventListener("click", () => {
   const result = playRound("scissors");
   resultDiv.textContent = result;
 });
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+  const result = resetGame();
+  resultDiv.textContent = result;
+});
+
